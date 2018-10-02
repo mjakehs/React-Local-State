413d058caf28befb819e0a4e49b54ecf00998091
Jake Marzitelli <mjakehs@gmail.com>
2018-10-02 13:58:36 -0500

    move header to component file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../logo.svg';
 
-class App extends Component {
+class Header extends Component {
   render() {
     return (
       <div className="App">
@@ -25,4 +24,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Header;
